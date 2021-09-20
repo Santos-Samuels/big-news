09b86bc478f87f348fc37e7355eb8f811c3a9c30
Santos-Samuels <saamsmith15@gmail.com>
2021-09-19 23:08:55 -0300

    deploy config

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,9 +44,15 @@ export default {
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
+  axios: {
+    
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+  },
+
+  generate: {
+    dir: 'public'
   }
 }
